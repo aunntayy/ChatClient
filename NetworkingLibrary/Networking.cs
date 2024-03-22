64598e777b9c215bba6d18bc0a5d79f1e1d95e0d
aunntayy <chanhkone@gmail.com>
2024-03-22 12:17:00 -0600

    added INetworking interface

---
+++ NetworkingLibrary/Networking.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Communications {
+    public class Networking : INetworking {
+        string INetworking.ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        bool INetworking.IsConnected => throw new NotImplementedException();
+
+        bool INetworking.IsWaitingForClients => throw new NotImplementedException();
+
+        string INetworking.RemoteAddressPort => throw new NotImplementedException();
+
+        string INetworking.LocalAddressPort => throw new NotImplementedException();
+
+        Task INetworking.ConnectAsync(string host, int port) {
+            throw new NotImplementedException();
+        }
+
+        void INetworking.Disconnect() {
+            throw new NotImplementedException();
+        }
+
+        Task INetworking.HandleIncomingDataAsync(bool infinite) {
+            throw new NotImplementedException();
+        }
+
+        Task INetworking.SendAsync(string text) {
+            throw new NotImplementedException();
+        }
+
+        void INetworking.StopWaitingForClients() {
+            throw new NotImplementedException();
+        }
+
+        void INetworking.StopWaitingForMessages() {
+            throw new NotImplementedException();
+        }
+
+        Task INetworking.WaitForClientsAsync(int port, bool infinite) {
+            throw new NotImplementedException();
+        }
+    }
+}
