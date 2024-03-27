ff6d366b81060494355fb53c9e93db20e2743d8f
aunntayy <chanhkone@gmail.com>
2024-03-26 22:38:49 -0700

    commit to pull from master

--- ChatClient/MainPage.xaml.cs
+++ ChatClient/MainPage.xaml.cs
@@ -1,12 +1,25 @@
-﻿namespace ChatClient
+﻿using Android.Opengl;
+using Microsoft.Maui.Controls;
+using static Android.Graphics.ColorSpace;
+
+namespace ChatClient
 {
     public partial class MainPage : ContentPage
     {
-      
 
-        public MainPage()
-        {
+        //Type in the name of the remote machine you wish to talk to(default to localhost)
+        //Type in the name of the chatter(e.g. "Jim")
+        //Type in a message(and have it sent to the server)
+        //Have a way to show who is currently on the server(participants)
+        //Have a list of the conversation.
+        //Show the status (Connected/Disconnected/Error/etc.)
+        private Networking _networking;
+
+
+
+        public MainPage(){
             InitializeComponent();
+            
         }
 
        
